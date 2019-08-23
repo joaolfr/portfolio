7758d294673e918e8af81316fa5e9c289e58b34e
Joao-Luiz-Freitas-Rodrigues <joaoluizfreitasrodrigues@gmail.com>
2019-08-23 09:57:51 -0300

    alteracoes iniciais

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./styles.css";
+
+const Header = () => (
+    <header id="main-header"> João Luiz Freitas Rodrigues</header>
+);
+
+export default Header;
+// Esse metodo cima substitiu o abaixo
+// 
+// class Header extends Component {
+//     render(){
+//         return(
+
+//         )
+//     }
+// }
