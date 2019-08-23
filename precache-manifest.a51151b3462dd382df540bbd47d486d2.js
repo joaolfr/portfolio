fce1281ffe73931beee51a009dad8b2b1404b977
Joao-Luiz-Freitas-Rodrigues <joaoluizfreitasrodrigues@gmail.com>
2019-08-23 15:36:28 -0300

    Updates

---
+++ precache-manifest.a51151b3462dd382df540bbd47d486d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a3e5c5add54eee99e48918feab04d8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "879ae3fa92b80ed02df9",
+    "url": "/portfolio/static/css/main.374cb235.chunk.css"
+  },
+  {
+    "revision": "389143019dfd22e9ef8f",
+    "url": "/portfolio/static/js/2.33dbb794.chunk.js"
+  },
+  {
+    "revision": "879ae3fa92b80ed02df9",
+    "url": "/portfolio/static/js/main.01884f04.chunk.js"
+  },
+  {
+    "revision": "e8aab94e553ef1acd8d3",
+    "url": "/portfolio/static/js/runtime~main.e9a507a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2180d47a2b9e66c5be5f4b3f4f63fbb6.js"
+  "/portfolio/precache-manifest.a51151b3462dd382df540bbd47d486d2.js"
 );
 
 self.addEventListener('message', (event) => {
